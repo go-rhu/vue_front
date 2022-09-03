6c9fc7c79409d09425ecedd6d6b00bd8074fb946
bob.fs <bob.fs@foxmail.com>
2022-09-03 10:54:16 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+
+
+<template>
+<div class="table-box">
+    <div class="title">
+        <h2>简单的CURD</h2>
+    </div>
+    <div class="query-box">
+        <el-input v-model="qryInput" placeholder="请输入姓名搜索" />
+        <el-button type="primary">增加</el-button>
+    </div>
+<!--标题-->
+<el-table :data="tableData" style="width: 100%" border>
+    <el-table-column fixed prop="date" label="Date" width="150" />
+    <el-table-column prop="name" label="Name" width="120" />
+    <el-table-column prop="state" label="State" width="120" />
+    <el-table-column prop="city" label="City" width="120" />
+    <el-table-column prop="address" label="Address" width="600" />
+    <el-table-column prop="zip" label="Zip" width="120" />
+    <el-table-column fixed="right" label="Operations" width="120">
+      <template #default>
+        <el-button link type="primary" size="small" @click="handleB1click"
+          >Detail</el-button
+        >
+        <el-button link type="primary" size="small">Edit</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</div>
+
+</template>
+
+<script setup>
+    //**数据
+    import{ref} from "vue";
+    let qryInput =ref("");
+    let tableData =ref([{
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Home',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+    tag: 'Office',
+  },])
+    //**方法
+    let handleB1click=()=>{
+        console.log("click")
+    }
+</script>
+<style scoped >
+.table-box{
+    width:800px;
+    position:absolute;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-50%);
+}
+.title{
+    text-align:center;
+}
+.query-box{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+}
+.el-input{
+    width:200px
+}
+</style>
